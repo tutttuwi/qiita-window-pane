edd1fc028fd80dcc58e00347b1bee500d1db8bf1
t-tsutsui <t.tsutsui9117@gmail.com>
2023-05-24 23:06:22 +0900

    [mod] view on qiita items page

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,29 +1,38 @@
 <script setup lang="ts">
-import { useToggle } from '@vueuse/core'
+// import { useToggle } from '@vueuse/core'
 import 'uno.css'
 import '../style.css'
 
-const [show, toggle] = useToggle(false)
-// const isShow:string = ref("true");
+// const [show, toggle] = useToggle(false)
+const isShow = ref<ValueOf<typeof Boolean>>(true);
 
 function getAccountProfileUrl() {
   return window.location.href.replaceAll(/\/items\/.*/g, "");
 }
 
-// function showToggle() {
-//   isShow.value = !isShow.value;
-//   isShow.
-// }
+function showToggle(e: Event) {
+  e.stopPropagation();
+  isShow.value = !isShow.value;
+}
+
+function closeWindow() {
+  isShow.value = false;
+}
 
 </script>
 
 <template>
-  <div class="fixed qiita-window-container m-5 z-100 flex items-start font-sans select-none leading-1em">
-    <div class="bg-white text-gray-800 rounded-lg shadow w-max h-min" p="x-4 y-2" m="y-auto r-2"
-      transition="opacity duration-300" :class="show ? 'opacity-100' : 'opacity-0'">
-      <iframe :src="getAccountProfileUrl()" frameborder="0" style="border: 0" width="800" height="600"></iframe>
-    </div>
-    <div class="flex w-10 h-10 rounded-full shadow cursor-pointer" @click="toggle()">
+  <div class="fixed qiita-window-container m-5 z-100 flex items-start justify-end font-sans select-none leading-1em">
+    <template v-if="isShow">
+      <div class="qiita-window-inner" @click="closeWindow()">
+      </div>
+      <div class="bg-white text-gray-800 rounded-lg shadow w-max h-min z-100" p="x-1 y-1" m="r-4 l-4"
+        transition="opacity duration-300">
+        <iframe class="list-scroll" :src="getAccountProfileUrl()" frameborder="0" style="border: 0" width="500"
+          height="800" scrolling="no"></iframe>
+      </div>
+    </template>
+    <div class="flex w-10 h-10 rounded-full shadow cursor-pointer" @click="showToggle($event)">
       <img title="[Qiitaノ窓]プロフィールを覗く(/ω・＼)ﾁﾗｯ" src="../../assets/icon-16.png" />
     </div>
   </div>
